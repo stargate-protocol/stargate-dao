a6b83fef33b1f0680c0afa11633927da400c8b14
Claudia <claudiabarcelovaldes40@gmail.com>
2025-09-18 16:10:22 +0200

    feat: add needed dependencies and config

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -6,9 +6,80 @@ import "hardhat-deploy-ethers"
 import { HardhatUserConfig } from "hardhat/config"
 import "solidity-coverage"
 import "dotenv/config"
+import type { NetworksUserConfig } from "hardhat/types"
+import { join } from "path"
+import "./task/get-users-accounts"
 
 function accounts() {
     return { mnemonic: process.env.MNEMONIC }
+}
+
+const networks: NetworksUserConfig = {
+    ethereum: {
+        url: process.env.RPC_URL_ETHEREUM,
+        chainId: 1,
+        accounts: accounts(),
+    },
+    bsc: {
+        url: process.env.RPC_URL_BSC,
+        chainId: 56,
+        accounts: accounts(),
+    },
+    avalanche: {
+        url: process.env.RPC_URL_AVALANCHE,
+        chainId: 43114,
+        accounts: accounts(),
+    },
+    polygon: {
+        url: process.env.RPC_URL_POLYGON,
+        chainId: 137,
+        accounts: accounts(),
+    },
+    arbitrum: {
+        url: process.env.RPC_URL_ARBITRUM,
+        chainId: 42161,
+        accounts: accounts(),
+    },
+    optimism: {
+        url: process.env.RPC_URL_OPTIMISM,
+        chainId: 10,
+        accounts: accounts(),
+    },
+    fantom: {
+        url: process.env.RPC_URL_FANTOM,
+        chainId: 250,
+        accounts: accounts(),
+    },
+}
+
+const externalConfig: HardhatUserConfig["external"] = {
+    deployments: Object.fromEntries(
+        Object.keys(networks).map((networkName) => [networkName, [join(__dirname, "deployments", networkName)]] as const)
+    ),
+}
+
+const updateNetworkRpcUrls = (networks: NetworksUserConfig): NetworksUserConfig => {
+    return Object.fromEntries(
+        Object.entries(networks).map(([networkName, networkConfig]) => {
+            if (networkConfig && "url" in networkConfig) {
+                // Only use dynamic URL if the static one is undefined and template exists
+                if (!networkConfig.url && process.env.RPC_URL_MAINNET) {
+                    const dynamicUrl = getRpcUrl(networkName)
+                    return [networkName, { ...networkConfig, url: dynamicUrl }]
+                }
+            }
+            return [networkName, networkConfig]
+        })
+    )
+}
+
+const getRpcUrl = (chainName: string): string | null => {
+    let templateUrl = process.env.RPC_URL_MAINNET
+    if (!templateUrl) return null
+    const url = templateUrl.replace("CHAIN", chainName)
+
+    console.log("====> url", url)
+    return url
 }
 
 const config: HardhatUserConfig = {
@@ -40,42 +111,9 @@ const config: HardhatUserConfig = {
         },
     },
     networks: {
-        ethereum: {
-            url: process.env.RPC_URL_ETHEREUM,
-            chainId: 1,
-            accounts: accounts(),
-        },
-        bsc: {
-            url: process.env.RPC_URL_BSC,
-            chainId: 56,
-            accounts: accounts(),
-        },
-        avalanche: {
-            url: process.env.RPC_URL_AVALANCHE,
-            chainId: 43114,
-            accounts: accounts(),
-        },
-        polygon: {
-            url: process.env.RPC_URL_POLYGON,
-            chainId: 137,
-            accounts: accounts(),
-        },
-        arbitrum: {
-            url: process.env.RPC_URL_ARBITRUM,
-            chainId: 42161,
-            accounts: accounts(),
-        },
-        optimism: {
-            url: process.env.RPC_URL_OPTIMISM,
-            chainId: 10,
-            accounts: accounts(),
-        },
-        fantom: {
-            url: process.env.RPC_URL_FANTOM,
-            chainId: 250,
-            accounts: accounts(),
-        },
+        ...updateNetworkRpcUrls(networks),
     },
+    external: externalConfig,
 }
 
 export default config
