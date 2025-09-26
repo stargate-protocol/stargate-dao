5daaea873cfd7b3caf9dabde0703828bf1701246
Claudia <claudiabarcelovaldes40@gmail.com>
2025-09-26 14:41:33 +0200

    feat: add optimisim data

--- scripts/feeDistributorClaims/constants.ts
+++ scripts/feeDistributorClaims/constants.ts
@@ -3,6 +3,7 @@ export const REWARD_TOKEN_BY_CHAIN: Record<string, string> = {
     ethereum: "0xA0b86991c6218b36c1d19D4a2e9Eb0cE3606eB48",
     avalanche: "0xB97EF9Ef8734C71904D8002F8b6Bc66Dd9c48a6E",
     bsc: "0x55d398326f99059fF775485246999027B3197955",
+    optimism: "0x7F5c764cBc14f9669B88837ca1490cCa17c31607",
 }
 
 export const FD_BY_CHAIN: Record<string, string> = {
@@ -10,6 +11,7 @@ export const FD_BY_CHAIN: Record<string, string> = {
     ethereum: "0xAF667811A7eDcD5B0066CD4cA0da51637DB76D09",
     avalanche: "0xAF667811A7eDcD5B0066CD4cA0da51637DB76D09",
     bsc: "0xAF667811A7eDcD5B0066CD4cA0da51637DB76D09",
+    optimism: "0xAF667811A7eDcD5B0066CD4cA0da51637DB76D09",
 }
 
 export const VE_BY_CHAIN: Record<string, string> = {
@@ -17,6 +19,7 @@ export const VE_BY_CHAIN: Record<string, string> = {
     ethereum: "0x0e42acBD23FAee03249DAFF896b78d7e79fBD58E",
     avalanche: "0xCa0F57D295bbcE554DA2c07b005b7d6565a58fCE",
     bsc: "0xD4888870C8686c748232719051b677791dBDa26D",
+    optimism: "0x43d2761ed16C89A2C4342e2B16A3C61Ccf88f05B",
 }
 
 export const INPUT_CSV_PATH = (chain: string) => `./scripts/feeDistributorClaims/data/${chain}/in/addresses.csv`
